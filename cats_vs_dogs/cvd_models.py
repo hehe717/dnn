9fb808ce74fb718c1958e18edc95b460b08dbb89
Wonjae.Na <nawonjae@naver.com>
2017-08-20 02:35:04 +0900

    d

--- cats_vs_dogs/cvd_models.py
+++ cats_vs_dogs/cvd_models.py
@@ -38,7 +38,7 @@ class LogisticModel(models.BaseModel):
     print "model_input"
     print model_input
 
-    net = slim.conv2d(model_input, 3, [2, 2], activation_fn=tf.nn.relu)
+    net = slim.conv2d(model_input, 5, [2, 2], activation_fn=tf.nn.relu)
     net = slim.max_pool2d(net, [2, 2], scope='pool2')
     net = slim.dropout(net, 0.2)
     net1 = net
@@ -51,7 +51,7 @@ class LogisticModel(models.BaseModel):
     net = slim.max_pool2d(net, [2, 2], scope='pool4')
     net = slim.dropout(net, 0.2)
 
-    net = slim.fully_connected(net, 32, activation_fn=tf.nn.relu,
+    net = slim.fully_connected(net, 40, activation_fn=tf.nn.relu,
                                weights_regularizer=slim.l2_regularizer(l2_penalty))
     net = slim.flatten(net)
     output = slim.fully_connected(
